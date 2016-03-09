2383bddb763ad86652b3f02a0ef051636ec55ca2
Eugene <ernestyj@outlook.com>
2016-03-09 19:19:41 +0800

    Chap9

--- src/com/eugene/impatience_scala/Chap9.scala
+++ src/com/eugene/impatience_scala/Chap9.scala
@@ -1,7 +1,8 @@
 package com.eugene.impatience_scala
 
-import java.io.{File, PrintWriter}
+import java.io._
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 /**
@@ -13,6 +14,8 @@ object Chap9 {
 //        t2()
 //        t3()
 //        t5()
+//        t9()
+//        t10()
     }
     def t1() = {
         val path = "resources/test.txt"
@@ -46,4 +49,43 @@ object Chap9 {
         pw.flush()
         pw.close()
     }
+    def t9() = {
+        var count = 0
+        for(d <- subdirs(new File("."))) {
+            d.listFiles().filter(_.isFile).foreach(f => if(f.getName.endsWith(".class")) println(f.getName))
+            count += d.listFiles().filter(_.isFile).count(_.getName.endsWith(".class"))
+        }
+        print(count)
+    }
+    def subdirs(dir:File):Iterator[File] = {
+        val children = dir.listFiles.filter(_.isDirectory)
+        children.toIterator ++ children.toIterator.flatMap(subdirs _)
+    }
+
+    class Person(var name:String) extends Serializable{
+        val friends = new ArrayBuffer[Person]()
+        def addFriend(friend : Person){
+            friends += friend
+        }
+        override def toString() = {
+            var str = "My name is " + name + " and my friends name is "
+            friends.foreach(str += _.name + ",")
+            str
+        }
+    }
+    def t10() = {
+        val p1 = new Person("Ivan")
+        val p2 = new Person("F2")
+        val p3 = new Person("F3")
+        p1.addFriend(p2)
+        p1.addFriend(p3)
+        println(p1)
+        val out = new ObjectOutputStream(new FileOutputStream("person.obj"))
+        out.writeObject(p1)
+        out.close()
+        val in =  new ObjectInputStream(new FileInputStream("person.obj"))
+        val p = in.readObject().asInstanceOf[Person]
+        println(p)
+    }
+
 }
