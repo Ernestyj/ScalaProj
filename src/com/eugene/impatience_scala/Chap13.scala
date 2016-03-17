c1017eb65c05cb9609a740b8bce7e57e3a159cea
Eugene <ernestyj@outlook.com>
2016-03-17 18:29:28 +0800

    Chap13

--- src/com/eugene/impatience_scala/Chap13.scala
+++ src/com/eugene/impatience_scala/Chap13.scala
@@ -29,14 +29,26 @@ object Chap13 {
         println((List[Int]() /: lst)(_ :+ _))
         println((lst :\ List[Int]())((i,list)=>list:+i))
         println((List[Int]() /: lst)((list,i)=>i::list))
+        println
         //7
-
+        val prices = List(5.0,20.0,9.95)
+        val quantities = List(10,2,1)
+        println((prices zip quantities) map { Function.tupled(_ * _) })
+        println
         //8
-
-        //9
-
+        toTwoDim(Array(1.0,2,3,4,5,6), 3).foreach(arr => println(arr.mkString(",")))
+        println
         //10
-        
+        val str = "abdcsdcd"
+        val frequencies = str.par.aggregate(immutable.HashMap[Char, Int]())(
+            {(map, k) => map+(k -> (map.getOrElse(k, 0)+1))},
+            {(map1, map2) =>
+                (map1.keySet++map2.keySet).foldLeft(immutable.HashMap[Char, Int]())(
+                    (resultMap, k) => resultMap+(k -> (map1.getOrElse(k, 0)+map2.getOrElse(k,0)))
+                )
+            }
+        )
+        println(frequencies.mkString(","))
     }
 
     def mapStrIndex(str:String) = {
@@ -73,5 +85,9 @@ object Chap13 {
 
     def myMkString(it:Iterable[String], split:String="") = {
         if (it!=Nil) it.reduceLeft(_+split+_)
+    }
+
+    def toTwoDim(arr:Array[Double], col:Int) = {
+        arr.grouped(col).toArray
     }
 }
